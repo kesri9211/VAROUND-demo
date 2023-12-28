f2954724bd381f41f79d600a813182f118b92904
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-28 23:14:01 +0530

    Create nav.jsx

---
+++ nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <>
+            <div className="navDiv">
+                <ul className="navUl">
+                    <li><a href="index.js" className="nav_a">About Us</a></li>
+                    <li><a href="index.js" className="nav_a">Feed</a></li>
+                    <li><a href="#" className="nav_a">Facility</a></li>
+                    <li><a href="#" className="nav_a">Home</a></li>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default Nav;
