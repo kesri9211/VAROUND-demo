d2d9830b988f7c81eb64fe16f29a62538465bbd5
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-28 23:16:00 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function About(){
+    return(
+        <>
+            <div className="about_div">
+            <h2>About Website</h2>
+            <p className="about_p">Lorem Ipsum is dummy text of the printing and typesetting industry, derived from a Latin passage by Cicero. Learn about its history, usage, variations and sources, and how to generate realistic Lorem Ipsum online.</p>
+            </div>
+            <div className="about_div">
+            <h2>About Us</h2>
+            <p className="about_p">Lorem Ipsum is dummy text of the printing and typesetting industry, derived from a Latin passage by Cicero. Learn about its history, usage, variations and sources, and how to generate realistic Lorem Ipsum online.</p>
+            </div>
+        </>
+    );
+}
+export default About;
